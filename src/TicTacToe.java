5982714a69b4d9140da1306e4a46d1a84ceb5c2c
jon <zhenagajo234@gmail.com>
2023-04-20 10:45:22 +0700

    add

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,50 +1,14 @@
-import javax.naming.spi.DirStateFactory.Result;
-import javax.swing.*;
-
-import org.w3c.dom.Text;
-
 import java.awt.*;
+import javax.swing.*;
+import java.util.*;
 import java.awt.event.*;
 
 
 public class TicTacToe implements ActionListener
 {
-    JFrame frame;
-    JTextField textField;
-    JButton[] button = new JButton[9];
-    JPanel panel;
-
-    Font myFont = new Font("Times New Roman",Font.BOLD,30);
-
-
     TicTacToe()
     {
-        frame = new JFrame("Tic Tac Toe");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(550,500);
-        frame.setLayout(null); // we can take full control of the GUI
 
-        textField = new JTextField();
-        textField.setBounds(37,25,450,50);
-        textField.setFont(myFont);
-        textField.setEditable(false);
-        textField.setBackground(Color.gray);
-
-        for(int i = 0;i < 9;i++)
-        {
-            button[i] = new JButton();
-            button[i].setFont(myFont);
-            button[i].addActionListener(this);
-        }
-
-        panel = new JPanel();
-        //panel.setBounds();
-        panel.setBackground(Color.red);
-
-
-        frame.add(textField);
-        frame.add(panel);
-        frame.setVisible(true);
     }
 
 
@@ -52,6 +16,25 @@ public class TicTacToe implements ActionListener
     {
 
     }
+
+    public void firstTurn()
+    {
+
+    }
+
+    public void check()
+    {
+
+    }
+
+    public void xWinds()
+    {
+        
+    }
+
+
+    
+    
     public static void main(String[] args)
     {
         TicTacToe m = new TicTacToe();
